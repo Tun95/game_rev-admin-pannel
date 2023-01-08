2582e176f628535e39f221beef9241f53e14dd7e
Ola Josh <akandetunji3@gmail.com>
2023-01-09 00:11:26 +0100

    new

--- src/screens/manage ads/Advertise.jsx
+++ src/screens/manage ads/Advertise.jsx
@@ -1,9 +1,13 @@
 import React from "react";
+import { Helmet } from "react-helmet-async";
 import Advert from "../../component/manage ads/Advert";
 
 function Advertise() {
   return (
     <div>
+      <Helmet>
+        <title>Manage Ads</title>
+      </Helmet>
       <Advert />
     </div>
   );
